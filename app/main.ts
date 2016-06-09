4f16484d4091196249793d9f90692ad0606194fc
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-06-09 08:33:54 -0500

    Added start of project.

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by robbieplankenhorn on 6/8/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
