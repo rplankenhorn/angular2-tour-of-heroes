a3aff01189fd156ae49f839d41fe1e3a80e4a337
Robbie Plankenhorn <rplankenhorn@gmail.com>
2016-06-10 08:38:25 -0500

    Completed Part 6

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -8,12 +8,15 @@ import { HeroService } from './hero.service';
 @Component({
     selector: 'my-heros',
     templateUrl: 'app/heroes.component.html',
-    styleUrls: ['app/heroes.component.css']
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error: any;
 
     constructor(
         private router: Router,
@@ -33,5 +36,27 @@ export class HeroesComponent implements OnInit {
 
     gotoDetail() {
         this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) {
+            this.getHeroes();
+        }
+    }
+
+    delete(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService.delete(hero).then(res => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) {
+                this.selectedHero = null;
+            }
+        }).catch(error => this.error = error);
     }
 }
